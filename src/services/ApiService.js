ecbb05523372406ddd181253738fa54eb5e712e8
DotDmitry <155758582+DotDmitry@users.noreply.github.com>
2025-04-11 17:38:15 +0300

    Update ApiService.js

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,19 +1,21 @@
-const apiUrl = 'https://dashboard.elering.ee/api';
+//const apiUrl = 'https://dashboard.elering.ee/api';
 
-export const getPriceData = async (from,until) => {
+const apiUrl = 'https://lively-bird-ec92.dvirus13.workers.dev/api';
 
+export const getPriceData = async (from, until) => {
+    // URLSearchParams automatically handles encoding the parameter values
     const data = new URLSearchParams({
         start: from,
         end: until,
     });
 
+    // No need for encodeURIComponent here
     const response = await fetch(`${apiUrl}/nps/price?${data}`);
-
     return await response.json();
 }
 
 export const getPriceCurrent = async () => {
-
+    // No need for encodeURIComponent here
     const response = await fetch(`${apiUrl}/nps/price/EE/current`);
     return await response.json();
 }
